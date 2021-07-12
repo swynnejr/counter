f42c0c9decff405603d16bde57a835af31bcdba7
swynnejr <saucersam@gmail.com>
2021-07-12 16:28:25 -0500

    basic counter working

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, redirect, session
 app = Flask(__name__)
-app.secret_key = 'keep it secret, keep it safe'
+app.secret_key = 'super secret counter key'
 
 @app.route('/')
 def counter():
-    
-    return render_template("index.html")
+    if 'count' not in session:
+        session['count'] = 0
+    else:
+        session['count'] +=1
+    print(session['count'])
+    return render_template(("index.html"), count=session['count'])
 
 # @app.route('/destroy_session')
 # def counter():
-    
+
 #     return redirect("/")
 
 
