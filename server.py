8f3ecdfdf7156ea19b57aaa5fb45d4c94e8098b5
swynnejr <saucersam@gmail.com>
2021-07-12 14:43:39 -0500

    html links through server

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
 
 @app.route('/')
 def counter():
     
-    return render_template("index.html", times = times)
+    return render_template("index.html")
 
-@app.route('/destroy_session')
-def counter():
+# @app.route('/destroy_session')
+# def counter():
     
-    return redirect("/")
+#     return redirect("/")
 
 
 
